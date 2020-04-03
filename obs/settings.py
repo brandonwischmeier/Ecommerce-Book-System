f6a3a446db810018d77a66d90b12668ad3596114
Brandon Wischmeier <bdw26588@uga.edu>
2020-04-03 11:01:56 -0400

    Added DB connectivity

--- obs/settings.py
+++ obs/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'obs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_bookstore',
+        'USER': 'admin',
+        'PASSWORD': 'BrandonWischmeier12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
